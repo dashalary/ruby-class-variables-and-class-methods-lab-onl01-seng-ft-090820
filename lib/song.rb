a023a71b2ed901444909e75d5bcc42a413a3e321
Dasha Lary <dk2800@nyu.edu>
2020-09-16 16:51:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0
+  @@new_count = 0
   @@genres = []
   @@artists = []
   @@genre_count = {}
@@ -29,7 +30,6 @@ class Song
   
   def self.genre_count 
     @@genres.each do |genre|
-      new_count = 0
       @@genre_count[genre] = new_count
       if @@genre_count.include?(genre)
         @@genre_count[genre] += 1
