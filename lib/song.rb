07abb5764f9a7f6217cb53a099a3588436561f4d
Dasha Lary <dk2800@nyu.edu>
2020-09-16 16:33:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,7 @@ class Song
   
   def self.genre_count 
     @@genres.each do |genre|
+      @@genre_count[genre] = new
       if @@genre_count.include?(genre) == false
         @@genre_count[genre] = 1 
       else
